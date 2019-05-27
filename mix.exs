44a5b3231bfba7f423c8d7672736ce4bd041f8d9
Dennis Ideler <di@rekki.com>
2019-05-27 17:10:29 +0100

    Update minimum elixir version from 1.4 to 1.6
    Benchee 1.0 is compatible with elixir ~> 1.6

--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule FastElixir.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :fast_elixir,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :fast_elixir,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
