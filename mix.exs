c85486645b8f833bcdb6e6b975737854b464094d
Dennis Ideler <di@rekki.com>
2019-05-27 17:09:43 +0100

    Update benchee from 0.14.0 to 1.0.1
    https://github.com/bencheeorg/benchee/blob/master/CHANGELOG.md#101-2019-04-09

--- mix.exs
+++ mix.exs
@@ -17,16 +17,8 @@ defmodule FastElixir.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  # Type "mix help deps" for more information
   defp deps do
-    [{:benchee, "~> 0.14"}]
+    [{:benchee, "~> 1.0"}]
   end
 end
