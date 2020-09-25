2f08a7958b699addb27a68e88efe7ac6b4b3aa72
Devon Estes <devon.c.estes@gmail.com>
2020-09-25 13:26:32 +0200

    update for elixir 1.11 and otp 23

--- code/general/sort_vs_sort_by.exs
+++ code/general/sort_vs_sort_by.exs
@@ -1,30 +1,35 @@
-defmodule Card, do: defstruct [:rank, :suit]
+defmodule Card, do: defstruct([:rank, :suit])
 
 defmodule Sort.Fast do
   def sort(enumerable), do: Enum.sort(enumerable)
 end
 
-defmodule Sort.Slow do
-  def sort(enumerable), do: Enum.sort(enumerable, &(&1.rank) <= (&2.rank))
+defmodule Sort.Medium do
+  def sort(enumerable), do: Enum.sort(enumerable, &(&1.rank <= &2.rank))
 end
 
-defmodule Sort.Slowest do
-  def sort(enumerable), do: Enum.sort_by(enumerable, &(&1.rank))
+defmodule Sort.Slow do
+  def sort(enumerable), do: Enum.sort_by(enumerable, & &1.rank)
 end
 
 defmodule Sort.Benchmark do
   def benchmark do
-    Benchee.run(%{
-      "sort/1" => fn -> bench(Sort.Fast) end,
-      "sort/2" => fn -> bench(Sort.Slow) end,
-      "sort_by/2" => fn -> bench(Sort.Slowest) end,
-    }, time: 10, print: [fast_warning: false])
+    Benchee.run(
+      %{
+        "sort/1" => fn -> bench(Sort.Fast) end,
+        "sort/2" => fn -> bench(Sort.Medium) end,
+        "sort_by/2" => fn -> bench(Sort.Slow) end
+      },
+      time: 10,
+      print: [fast_warning: false]
+    )
   end
 
   defp bench(module) do
-    cards = Enum.map 1..100, fn _ ->
-      %Card{rank: Enum.random(0..100), suit: Enum.random(~w[red green blue]a)}
-    end
+    cards =
+      Enum.map(1..100, fn _ ->
+        %Card{rank: Enum.random(0..100), suit: Enum.random(~w[red green blue]a)}
+      end)
 
     module.sort(cards)
   end
